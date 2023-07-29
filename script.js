118efcd67fe5486de37a2150362b5617ee909308
asil <9745019+asiill@users.noreply.github.com>
2023-07-29 13:57:13 -0400

    Write addBookToLibrary and isInLibrary functions. Create getBookFromInput function

--- script.js
+++ script.js
@@ -7,15 +7,34 @@ function Book(title, author, isRead) {
 }
 
 const bookForm = document.getElementById("book-form");
+
 const newBookBtn = document.getElementById("new-book-btn");
+
+const errorMessage = document.getElementById("error-message");
 const libraryContainer = document.getElementById("library-container");
+
+function getBookFromInput() {
+    let title = bookForm.title.value;
+    let author = bookForm.author.value;
+    let isRead = bookForm.isRead.checked;
+    let newBook = new Book(title, author, isRead);
+    return newBook;
+}
+
+function createBookContainer(book) {
+
+}
 
 function updateLibraryContainer() {
 
 }
 
-function isInLibrary(book) {
+function toggleStatus(book) {
 
+}
+
+function isInLibrary(newBook) {
+    return library.some(book => book.title === newBook.title && book.author === newBook.author);
 }
 
 function removeBookFromLibrary(book) {
@@ -23,5 +42,17 @@ function removeBookFromLibrary(book) {
 }
 
 function addBookToLibrary() {
-
+    let newBook = getBookFromInput();
+    if (isInLibrary(newBook)) {
+        errorMessage.textContent = "* This book is already in the library";
+        return;
+    } else {
+        library.push(newBook);
+        console.log(newBook);
+    }
 }
+
+bookForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    addBookToLibrary();
+});
