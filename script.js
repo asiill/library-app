1d8acdd571b6f19baea748d078f468084affaabe
asil <9745019+asiill@users.noreply.github.com>
2023-07-29 12:58:23 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let library = [];
+
+function Book(title, author, isRead) {
+    this.title = title;
+    this.author = author;
+    this.isRead = isRead;
+}
+
+const bookForm = document.getElementById("book-form");
+const newBookBtn = document.getElementById("new-book-btn");
+const libraryContainer = document.getElementById("library-container");
+
+function updateLibraryContainer() {
+
+}
+
+function isInLibrary(book) {
+
+}
+
+function removeBookFromLibrary(book) {
+    
+}
+
+function addBookToLibrary() {
+
+}
